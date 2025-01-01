360b90ea0deffd9466687c199ac27d3674e45d02
Virusmint <davidz1015@hotmail.com>
2025-01-01 02:22:20 -0500

    Tab width handled by some plugin I forgor

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -2,8 +2,8 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
-vim.opt.tabstop = 4 -- Number of visual spaces per tab
-vim.opt.shiftwidth = 4 -- Number of spaces to use for each step of (auto)indent
-vim.opt.expandtab = true -- Use spaces instead of tabs
+-- vim.opt.tabstop = 4 -- Number of visual spaces per tab
+-- vim.opt.shiftwidth = 4 -- Number of spaces to use for each step of (auto)indent
+-- vim.opt.expandtab = true -- Use spaces instead of tabs
 
 vim.g.ai_cmp = false -- Disable Copilot completion in favor of inline suggestions
