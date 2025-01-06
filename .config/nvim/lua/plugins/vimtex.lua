9815edc97e0adc5c63b062e5058b3169b49e91a0
Virusmint <davidz1015@hotmail.com>
2025-01-05 23:58:57 -0500

    vimtex support for macOS silicon

--- .config/nvim/lua/plugins/vimtex.lua
+++ .config/nvim/lua/plugins/vimtex.lua
@@ -3,14 +3,21 @@ return {
     "lervag/vimtex",
     lazy = false,
     config = function()
-      vim.g.vimtex_view_method = "zathura"
+      local system = vim.fn.system("uname")
+      system = system:gsub("%s+", "")
+
+      if system == "Darwin" then -- macOS
+        vim.g.vimtex_view_method = "skim" -- Zathura hardly works on macOS silicon
+      elseif system == "Linux" then
+        vim.g.vimtex_view_method = "zathura"
+      end
       vim.g.vimtex_compiler_method = "latexmk"
       vim.g.vimtex_compiler_latexmk = {
         options = {
           "-verbose",
           "-file-line-error",
-          "-auxdir=build", -- bundle aux files into build folder
-          "-synctex=1", -- for forward-search
+          "-auxdir=build",
+          "-synctex=1",
           "-interaction=nonstopmode",
           "-shell-escape",
         },
