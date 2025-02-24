48fd0229a30c140748d5818ad00ffe3749050845
Virusmint <davidz1015@hotmail.com>
2025-02-24 13:49:50 -0500

    vimtex auxdir fix

--- .config/nvim/lua/plugins/vimtex.lua
+++ .config/nvim/lua/plugins/vimtex.lua
@@ -14,10 +14,10 @@ return {
       end
       vim.g.vimtex_compiler_method = "latexmk"
       vim.g.vimtex_compiler_latexmk = {
-        aux_dir = "build",
         options = {
           "-verbose",
           "-file-line-error",
+          "-auxdir=build",
           "-synctex=1",
           "-interaction=nonstopmode",
           "-shell-escape",
