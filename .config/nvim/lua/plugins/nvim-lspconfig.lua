c87c0ce68c0f6e1fbdae2e9b4c8044d4e40ac6b8
Virusmint <davidz1015@hotmail.com>
2025-07-04 00:17:28 -0400

    lspconfig disable texlab inlayHints.labelReferences

--- .config/nvim/lua/plugins/nvim-lspconfig.lua
+++ .config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -5,10 +5,26 @@ return {
       local keys = require("lazyvim.plugins.lsp.keymaps").get()
       keys[#keys + 1] = { "<C-k>", mode = { "i" }, false }
 
+      -- Disable tex inlay hints (particularly for texlab lsp)
       vim.list_extend(opts.inlay_hints.exclude, {
         "tex",
       })
+
       opts.setup = {
+        -- https://github.com/latex-lsp/texlab/wiki/Configuration
+        texlab = function(_, opts)
+          require("lspconfig").texlab.setup({
+            settings = {
+              texlab = {
+                inlayHints = {
+                  labelReferences = false,
+                },
+              },
+            },
+          })
+          return true
+        end,
+        -- https://docs.astral.sh/ruff/editors/setup/#neovim
         pyright = function(_, opts)
           require("lspconfig").pyright.setup({
             settings = {
