04a56ffdbb818b6c781b6ba218fb9cfefede7c50
Virusmint <davidz1015@hotmail.com>
2025-08-22 16:22:43 -0400

    fish config add macos conda and macport to path

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -6,6 +6,10 @@ set fish_greeting
 
 if test (uname -s) = Darwin
     fish_add_path /opt/homebrew/bin
+    fish_add_path /opt/local/bin /opt/local/sbin
+    if test -f /Users/david/miniconda3/bin/conda
+        eval /Users/david/miniconda3/bin/conda "shell.fish" hook $argv | source
+    end
 else if test (uname -s) = Linux
     # pass for now
 end
@@ -14,12 +18,12 @@ set -x EDITOR nvim
 set -x VISUAL nvim
 
 alias gg=lazygit
-alias dot='gg --git-dir="$HOME/.dotfiles/" --work-tree="$HOME"'
-alias dotf='/usr/bin/git --git-dir="$HOME/.dotfiles/" --work-tree="$HOME"'
 alias gd=lazydocker
 alias gp="DOCKER_HOST=unix:///run/user/1000/podman/podman.sock lazydocker"
-alias n=nvim
 alias gitsane="git config pull.rebase true && git config rebase.autostash true"
+alias dot='gg --git-dir="$HOME/.dotfiles/" --work-tree="$HOME"'
+alias dotf='/usr/bin/git --git-dir="$HOME/.dotfiles/" --work-tree="$HOME"'
+alias n=nvim
 alias l="eza --long --all --group --header --icons"
 alias t="tree -C"
 alias cat="bat"
