094461465075ddc30bcedd02b2aaee17779781c9
Virusmint <davidz1015@hotmail.com>
2026-02-17 21:07:15 -0500

    nvim QOL autocmds

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -5,11 +5,6 @@
 local autocmd = vim.api.nvim_create_autocmd
 local augroup = vim.api.nvim_create_augroup
 
-autocmd("FileType", {
-  pattern = { "markdown", "tex", "rnoweb" },
-  command = "setlocal wrap spell",
-})
-
 -- Favor Pyright textDocument/hover: https://docs.astral.sh/ruff/editors/setup/#neovim
 autocmd("LspAttach", {
   group = augroup("lsp_attach_disable_ruff_hover", { clear = true }),
@@ -43,9 +38,24 @@ autocmd("BufRead", {
   desc = "Load custom LuaSnip snippets based on the course in the file path",
 })
 
+-- Disable nvim-treesitter syntax highlighting for Rnoweb. LaTeX already handled by Vimtex
+autocmd("FileType", {
+  pattern = { "rnoweb" },
+  callback = function()
+    -- Kill Treesitter
+    vim.treesitter.stop()
+  end,
+})
+
+-- Wrap and enable spellcheck for markdown, tex, and rnoweb files
+autocmd("FileType", {
+  pattern = { "markdown", "tex", "rnoweb" },
+  command = "setlocal wrap spell",
+})
+
 -- C shiftwidth to 4
 vim.api.nvim_create_autocmd("FileType", {
-  pattern = "c",
+  pattern = { "c", "cpp" },
   callback = function()
     vim.opt_local.shiftwidth = 4
   end,
